f885aff548dc3f8f8574e475a8d2042097bea5c0
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-12 22:40:16 -0300

    criando tipos de feedbacks no WidgetForm

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,5 +1,18 @@
 import CloseButton from "./CloseButton"
 
+const FeedbackTypes = {
+    BUG: {
+        title: "Problema",
+    },
+    IDEA: {
+        title: "Ideia",
+    },
+    OTHER: {
+        title: "Outro",
+    }
+}
+    
+
 export default function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex  flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -7,7 +20,11 @@ export default function WidgetForm() {
                 <span className="text-xl leading-6">Deixe seu Feedback</span>
            <CloseButton/>
            </header>
-              <p>Hello World</p>
+
+              <div className="text-xs text-neutral-400 w-full">
+                
+              </div>
+              
            <footer className="text-xs text-natural-400">
                 Feito para <a  className="underline underline-offset-1" href="">mim</a> 
            </footer>

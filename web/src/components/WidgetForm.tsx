c1f5d4f531bc5e2ad0bc58d7237f577dcee5b4d0
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-12 22:16:46 -0300

    Criando estrutura do Footer e header no WidgetForm

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,13 @@
 export default function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex  flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            Hello world
+           <header>
+                <span className="text-xl leading-6">Deixe seu Feedback</span>
+           </header>
+              <p>Hello World</p>
+           <footer className="text-xs text-natural-400">
+                Feito para <a  className="underline underline-offset-1" href="">mim</a> 
+           </footer>
         </div>
     )
      

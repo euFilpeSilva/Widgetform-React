9c3779acee51d53cef523bbed9eb177d20ba8735
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-14 00:44:52 -0300

    parametrizando escolha do tipo de Feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import CloseButton from "./CloseButton"
 import bugImageUrl from "../assets/bug.svg"
 import ideaImageUrl from "../assets/idea.svg"
 import thoughtImageUrl from "../assets/thought.svg"
+import { useState } from "react"
 
 
 const FeedbackTypes = {
@@ -29,8 +30,12 @@ const FeedbackTypes = {
     },
 }
 
+type FeedbackType = keyof typeof FeedbackTypes;
 
 export default function WidgetForm() {
+    const [ feedbackType, setFeedbackType ] = useState<FeedbackType | null>(null)
+    
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
            <header>
@@ -38,18 +43,22 @@ export default function WidgetForm() {
            <CloseButton/>
            </header>
 
-              <div className="flex py-8 gap-2 w-full">
-                { Object.entries(FeedbackTypes).map(([key, value]) => {
-                    return (
-                        <button
-                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                        >
-                            <img src={value.image.source} alt={value.image.source} />
-                            <span>{value.title}</span>
-                        </button>
-                    );
-                })}
-              </div>
+             {!feedbackType ? (
+                 <div className="flex py-8 gap-2 w-full">
+                 { Object.entries(FeedbackTypes).map(([key, value]) => {
+                     return (
+                         <button
+                          key={key}
+                          onClick={() => setFeedbackType(key as FeedbackType)} 
+                         className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                         >
+                             <img src={value.image.source} alt={value.image.source} />
+                             <span>{value.title}</span>
+                         </button>
+                     );
+                 })}
+               </div>
+             ): <p>Hello World</p>}
               
            <footer className="text-xs text-natural-400">
                 Feito para <a  className="underline underline-offset-1" href="">mim</a> 

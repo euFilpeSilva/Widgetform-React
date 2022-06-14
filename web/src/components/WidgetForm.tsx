db5f7d9266ee9beeb4ac4ae4febc2e700bb07ef5
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-13 23:53:32 -0300

    Ajustando e stilizando borão de Feedback .

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -32,16 +32,18 @@ const FeedbackTypes = {
 
 export default function WidgetForm() {
     return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex  flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
            <header>
                 <span className="text-xl leading-6">Deixe seu Feedback</span>
            <CloseButton/>
            </header>
 
-              <div className="text-xs text-neutral-400 w-full">
+              <div className="flex py-8 gap-2 w-full">
                 { Object.entries(FeedbackTypes).map(([key, value]) => {
                     return (
-                        <button>
+                        <button
+                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                        >
                             <img src={value.image.source} alt={value.image.source} />
                             <span>{value.title}</span>
                         </button>

56fd93363728b6bc7facf5ab6104626dca1d9d80
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-16 18:22:21 -0300

    Adicionando cor mais clara no tailwind config

--- web/src/components/WidgetForm/Steps/FedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FedbackContentStep.tsx
@@ -30,6 +30,14 @@ export default function FeedbackContentStep({feedbackType,
             className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent  rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
             placeholder="Conte com detalhes o que esta acontecendo..."
             />
+            <footer>
+                <button
+                type="submit"
+                className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm"
+                >
+
+                </button>
+            </footer>
         </form>
         </>
     )

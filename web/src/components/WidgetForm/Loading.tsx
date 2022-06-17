fe72b7ebce33365e727deb4b86d5d1681fc1eebf
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-17 19:14:40 -0300

    Criando component de Loading, trabalhando print

---
+++ web/src/components/WidgetForm/Loading.tsx
@@ -0,0 +1,8 @@
+import { Circle, CircleNotch } from "phosphor-react";
+
+export default function Loading() {
+    return (
+        <CircleNotch weight="bold" className="w-4 h-4" />
+    )
+        
+}
--- web/src/components/WidgetForm/ScreenShotButton.tsx
+++ web/src/components/WidgetForm/ScreenShotButton.tsx
@@ -1,17 +1,18 @@
 import html2canvas from "html2canvas";
 import { Camera } from "phosphor-react";
 import { useState } from "react";
+import Loading from "./Loading";
 
 export default function ScreenshotButton() {
-    const [isTakingScreenshotButton, setIsTakingScreenshotButton] = useState(false);
+    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
 
    async function handleTakeScreenShot() {
-        setIsTakingScreenshotButton(true);
+        setIsTakingScreenshot(true);
 
     const canvas = await html2canvas(document.querySelector('html')!); //tira um print
     const base64image = canvas.toDataURL('image/png');  // converte print pra png no formato base64 que é em formato de texto e representa uma imagem
 
-    setIsTakingScreenshotButton(false);
+    setIsTakingScreenshot(false);
     }
     return (
         <button
@@ -19,7 +20,8 @@ export default function ScreenshotButton() {
         onClick={handleTakeScreenShot}
         className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus-outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offfset-zinc-900 focus:ring-brand-500"
         >
-            <Camera className="w-6 h-6"/>
+            { isTakingScreenshot ? <Loading/>: <Camera className="w-6 h-6"/> }
         </button>
     )
 }
+

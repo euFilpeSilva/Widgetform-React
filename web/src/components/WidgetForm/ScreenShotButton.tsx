5f4e0743987ec375b8debae27d387e342a6c6d39
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-17 07:36:26 -0300

    Criando funç de printar tela comp SreenShotButton

--- web/src/components/WidgetForm/ScreenShotButton.tsx
+++ web/src/components/WidgetForm/ScreenShotButton.tsx
@@ -1,9 +1,15 @@
+import html2canvas from "html2canvas";
 import { Camera } from "phosphor-react";
 
 export default function ScreenshotButton() {
+   async function handleTakeScreenShot() {
+    const canvas = await html2canvas(document.querySelector('html')!); //tira um print
+    const base64image = canvas.toDataURL('image/png');  // converte print pra png no formato base64 que é em formato de texto e representa uma imagem
+    }
     return (
         <button
         type="button"
+        onClick={handleTakeScreenShot}
         className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus-outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offfset-zinc-900 focus:ring-brand-500"
         >
             <Camera className="w-6 h-6"/>

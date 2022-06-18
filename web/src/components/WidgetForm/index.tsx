c1d7356da1c70dd0db235597b33b09de08a0ffd1
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-06-17 22:45:55 -0300

    criando e stilizando tela de sucesso

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -41,13 +41,14 @@ export default function WidgetForm() {
     const [feedbackSent, setFeedbackSent] = useState(false)
     
     function handlleRestartFeedback() {
+        setFeedbackSent(false)
         setFeedbackType(null)
     }
     
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             { feedbackSent ? (
-                <FeedbackSuccessStep />
+                <FeedbackSuccessStep onFeedbackRestartRequested={handlleRestartFeedback} />
             ): (
                <>
                 {!feedbackType ? (

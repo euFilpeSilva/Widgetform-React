f1b09ccd98c8c9f940dfb6062f72fbe753e55eb2
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-09-26 13:07:46 -0300

    testando conectividade do remoto com git

--- src/components/WidgetForm/Steps/FedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FedbackContentStep.tsx
@@ -17,7 +17,7 @@ export default function FeedbackContentStep({feedbackType,
     const [comment, setComment] = useState(""); // Quando é digitado qualquer coisa na textarea o valor desse estado é atualizado
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
-
+        
        function handleSubmitFeedback(event: FormEvent) {
         event.preventDefault();
 
@@ -31,18 +31,18 @@ export default function FeedbackContentStep({feedbackType,
     return (
         <>
         <header>
-            <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+            <button type="button" className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
              onClick={onFeedbackRestartRequested}
             >
                 <ArrowLeft weight="bold" className="w-4 h-4"/>
             </button>
-        <span className="text-xl leading-6 flex items-center gap-2">
+        <span className="flex items-center gap-2 text-xl leading-6">
             <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
             {feedbackTypeInfo.title}
         </span>
          <CloseButton/>
         </header>
-        <form onSubmit={(handleSubmitFeedback)} className="my-4 w-full">
+        <form onSubmit={(handleSubmitFeedback)} className="w-full my-4">
             <textarea
             className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent  rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
             placeholder="Conte com detalhes o que esta acontecendo..."
@@ -56,7 +56,7 @@ export default function FeedbackContentStep({feedbackType,
                 <button
                 type="submit"
                 disabled={!comment}
-                className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus-outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offfset-zinc-900 focus:ring-brand-500 transittion-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                className="flex items-center justify-center flex-1 p-2 text-sm border-transparent rounded-md bg-brand-500 hover:bg-brand-300 focus-outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offfset-zinc-900 focus:ring-brand-500 transittion-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                 >
                     Enviar Feedback  
                 </button>
